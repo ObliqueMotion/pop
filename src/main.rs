41963e8cf9722aed917a9b10e2e4dd2f40c99954
Erik Nordin <erik.nordin@lt40031.local>
2019-06-13 14:55:36 -0700

    Uploaded Initial Project

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use lalrpop_util::lalrpop_mod;
+
+lalrpop_mod!(pub calculator1);
+
+#[test]
+fn calculator1() {
+    assert!(calculator1::TermParser::new().parse("22").is_ok());
+    assert!(calculator1::TermParser::new().parse("(22)").is_ok());
+    assert!(calculator1::TermParser::new().parse("((((22))))").is_ok());
+    assert!(calculator1::TermParser::new().parse("((22)").is_err());
+}
+
+fn main() {
+    println!("Hello, world!");
+    calculator1::TermParser::new().parse("((22)").unwrap();
+}
